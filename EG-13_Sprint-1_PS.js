9e2115224bfa19106a0be638da31b78b7e8c83d5
Osman Goni <osman.mirpur55@gmail.com>
2026-08-22 21:45:14 +0600

    find median

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -168,4 +168,28 @@ function mergeSortedArrays(arr1, arr2){
     
     return result;
 }
-console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMedian(nums){
+   // Handle empty array
+    if (nums.length === 0) {
+        return null;
+    }
+    
+    // Sort the array in ascending order
+    const sorted = [...nums].sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    
+    // If array length is odd, return the middle element
+    if (sorted.length % 2 === 1) {
+        return sorted[mid];
+    }
+    
+    // If array length is even, return average of two middle elements
+    return (sorted[mid - 1] + sorted[mid]) / 2;
+}
+
